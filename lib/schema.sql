28ece65c352e56a1de60cef0a1b4308febc87982
Brad Alexander <brad.alexander913@gmail.com>
2019-10-20 21:06:02 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, subgenre_id TEXT);
+CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, subgenre_id INTEGER);
 CREATE TABLE subgenres (id INTEGER PRIMARY KEY, name TEXT);
 CREATE TABLE authors (id INTEGER PRIMARY KEY, name TEXT);
 CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER);
