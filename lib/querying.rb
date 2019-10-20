d26fef50ee174838ba606c4dfbc3e2e1ba192c7a
Brad Alexander <brad.alexander913@gmail.com>
2019-10-20 21:11:40 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,11 +1,10 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year FROM brooks JOIN series ON books.series_id = series.id ORDER BY books.year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
   "Write your SQL query here"
 end
-
 
 def select_value_and_count_of_most_prolific_species
   "Write your SQL query here"
